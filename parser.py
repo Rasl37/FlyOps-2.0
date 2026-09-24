91a17dcd225739d4a2f712c9b21de35e4ad50704
Rasl37 <mr.kolyda05@gmail.com>
2026-09-24 13:10:24 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,19 +7,15 @@ df_conn = pd.read_csv(
     usecols=['pre_root_id', 'post_root_id', 'syn_count']
 )
 
-# Фильтруем шум: берем связи с достаточным весом
 df_strong = df_conn[df_conn['syn_count'] >= 50]
 
-# Берем самый активный узел-источник как стартовую точку
 start_node = df_strong['pre_root_id'].value_counts().index[0]
 
-# Собираем цепочку в 3 шага (hops)
 selected_nodes = {start_node}
 chain_edges = []
 
 current_layer = [start_node]
 for step in range(3):
-    # Ищем, куда идут сигналы от текущего слоя
     next_hops = df_strong[df_strong['pre_root_id'].isin(current_layer)]
     top_hops = next_hops.sort_values(by='syn_count', ascending=False).head(8)
 
@@ -32,7 +28,6 @@ for step in range(3):
 
     current_layer = list(top_hops['post_root_id'].unique())
 
-# Добавляем альтернативные/обходные связи между уже отобранными узлами
 extra_edges = df_strong[
     df_strong['pre_root_id'].isin(selected_nodes) &
     df_strong['post_root_id'].isin(selected_nodes)
@@ -43,7 +38,6 @@ all_edges_df = pd.concat([pd.DataFrame(chain_edges), extra_edges]).drop_duplicat
 
 print(f"Отобрано узлов в контуре: {len(selected_nodes)}, связей: {len(all_edges_df)}")
 
-# 2. Формируем граф
 nodes = [{"id": f"neuron-{str(n)[-4:]}", "full_id": str(n)} for n in selected_nodes]
 edges = [
     {
